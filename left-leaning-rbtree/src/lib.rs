9bf978450b56d8877a584c97e8bea23c6c84c875
Nagata Kana <ngtkana@gmail.com>
2025-11-03 00:48:21 +0900

    refactor llrb

--- left-leaning-rbtree/src/lib.rs
+++ left-leaning-rbtree/src/lib.rs
@@ -1,4 +1,7 @@
-use std::{cmp::Ordering, ptr::null_mut};
+use std::{
+    cmp::Ordering::{self, *},
+    ptr::null_mut,
+};
 use Color::{Black, Red};
 
 pub struct Rbtree {
@@ -12,29 +15,12 @@ impl Default for Rbtree {
 }
 
 impl Rbtree {
-    pub fn insert(&mut self, key: i64) -> bool {
-        unsafe {
-            let inserted;
-            (self.root, inserted) = insert_impl(self.root, key);
-            (*self.root).color = Black;
-            inserted
-        }
+    pub fn insert(&mut self, key: i64) {
+        unsafe { self.root = insert(self.root, key) }
     }
 
-    pub fn remove(&mut self, key: i64) -> bool {
-        unsafe {
-            let mut removed = null_mut();
-            if !self.root.is_null() {
-                if color((*self.root).left) == Color::Black {
-                    (*self.root).color = Red;
-                }
-                (self.root, removed) = remove_impl(&mut *self.root, key);
-                if !self.root.is_null() {
-                    (*self.root).color = Black;
-                }
-            }
-            !removed.is_null()
-        }
+    pub fn remove(&mut self, key: i64) {
+        unsafe { self.root = remove(self.root, key) }
     }
 }
 
@@ -44,12 +30,8 @@ enum Color {
     Black,
 }
 
-unsafe fn color(node: *const Node) -> Color {
-    if let Some(node) = node.as_ref() {
-        node.color
-    } else {
-        Black
-    }
+unsafe fn color(x: *const Node) -> Color {
+    x.as_ref().map_or(Black, |x| x.color)
 }
 
 pub struct Node {
@@ -59,106 +41,122 @@ pub struct Node {
     color: Color,
 }
 
-unsafe fn insert_impl(root: *mut Node, key: i64) -> (*mut Node, bool) {
-    let Some(root) = root.as_mut() else {
-        return (
-            Box::leak(Box::new(Node {
-                left: null_mut(),
-                right: null_mut(),
-                key,
-                color: Color::Red,
-            })),
-            true,
-        );
-    };
-    let inserted;
-    match root.key.cmp(&key) {
-        Ordering::Greater => (root.left, inserted) = insert_impl(root.left, key),
-        Ordering::Equal => return (root, false),
-        Ordering::Less => (root.right, inserted) = insert_impl(root.right, key),
-    }
-    (fix_up(root), inserted)
+unsafe fn insert(mut x: *mut Node, key: i64) -> *mut Node {
+    x = insert_recurse(x, key);
+    (*x).color = Black;
+    x
 }
 
-unsafe fn remove_impl(mut root: &mut Node, key: i64) -> (*mut Node, *mut Node) {
-    let mut removed = null_mut();
-    match root.key.cmp(&key) {
+unsafe fn insert_recurse(x: *mut Node, key: i64) -> *mut Node {
+    let Some(x) = x.as_mut() else {
+        return Box::leak(Box::new(Node {
+            left: null_mut(),
+            right: null_mut(),
+            key,
+            color: Red,
+        }));
+    };
+    match x.key.cmp(&key) {
+        Greater => x.left = insert_recurse(x.left, key),
+        Equal => return x,
+        Less => x.right = insert_recurse(x.right, key),
+    }
+    fixup(x)
+}
+
+unsafe fn remove(x: *mut Node, key: i64) -> *mut Node {
+    let Some(x) = x.as_mut() else {
+        return null_mut();
+    };
+    if color(x.left) == Color::Black {
+        x.color = Red;
+    }
+    let x = remove_recurse(&mut *x, key);
+    if let Some(x) = x.as_mut() {
+        x.color = Black;
+    }
+    x
+}
+
+unsafe fn remove_recurse(mut x: &mut Node, key: i64) -> *mut Node {
+    match x.key.cmp(&key) {
         Ordering::Greater => {
-            let Some(left) = root.left.as_mut() else {
-                return (root, null_mut());
+            let Some(l) = x.left.as_mut() else {
+                return x;
             };
-            if left.color == Black && color(left.left) == Black {
-                root = move_red_left(root);
+            if l.color == Black && color(l.left) == Black {
+                x = move_red_left(x);
             }
-            (root.left, removed) = remove_impl(&mut *root.left, key);
+            x.left = remove_recurse(&mut *x.left, key);
         }
         cmp @ (Ordering::Equal | Ordering::Less) => {
-            if color(root.left) == Red {
-                root = rotate_right(root);
+            if color(x.left) == Red {
+                x = rotate_right(x);
             }
-            let Some(right) = root.right.as_mut() else {
+            let Some(r) = x.right.as_mut() else {
                 return match cmp {
                     Ordering::Greater => unreachable!(),
-                    Ordering::Equal => (null_mut(), root),
-                    Ordering::Less => (root, null_mut()),
+                    Ordering::Equal => null_mut(),
+                    Ordering::Less => x,
                 };
             };
-            if right.color == Black && color(right.left) == Black {
-                root = move_red_right(root);
+            if r.color == Black && color(r.left) == Black {
+                x = move_red_right(x);
             }
-            if root.key == key {
-                (root.right, removed) = remove_min(&mut *root.right);
-                root.key = (*removed).key;
-            } else if !root.right.is_null() {
-                (root.right, removed) = remove_impl(&mut *root.right, key);
+            if x.key == key {
+                let removed;
+                (x.right, removed) = remove_min(&mut *x.right);
+                x.key = (*removed).key;
+            } else if !x.right.is_null() {
+                x.right = remove_recurse(&mut *x.right, key);
             }
         }
     }
-    (fix_up(root), removed)
+    fixup(x)
 }
 
-unsafe fn move_red_left(mut root: &mut Node) -> &mut Node {
-    join_two_nodes(root);
-    if color((*root.right).left) == Color::Red {
-        root.right = rotate_right(&mut *root.right);
-        root = rotate_left(root);
-        split_four_node(root);
+unsafe fn remove_min(mut x: &mut Node) -> (*mut Node, *mut Node) {
+    if x.left.is_null() {
+        return (null_mut(), x);
     }
-    root
+    if color(x.left) == Black && color((*x.left).left) == Black {
+        x = move_red_left(x);
+    }
+    let removed;
+    (x.left, removed) = remove_min(&mut *x.left);
+    (fixup(x), removed)
 }
 
-unsafe fn move_red_right(mut root: &mut Node) -> &mut Node {
-    join_two_nodes(root);
-    if color((*root.left).left) == Red {
-        root = rotate_right(root);
-        split_four_node(root);
+unsafe fn move_red_left(mut x: &mut Node) -> &mut Node {
+    join_two_nodes(x);
+    if color((*x.right).left) == Color::Red {
+        x.right = rotate_right(&mut *x.right);
+        x = rotate_left(x);
+        split_four_node(x);
     }
-    root
+    x
 }
 
-unsafe fn fix_up(mut root: &mut Node) -> &mut Node {
-    if color(root.left) == Black && color(root.right) == Red {
-        root = rotate_left(root);
+unsafe fn move_red_right(mut x: &mut Node) -> &mut Node {
+    join_two_nodes(x);
+    if color((*x.left).left) == Red {
+        x = rotate_right(x);
+        split_four_node(x);
     }
-    if color(root.left) == Red && color((*root.left).left) == Red {
-        root = rotate_right(root);
-    }
-    if color(root.left) == Red && color(root.right) == Red {
-        split_four_node(root);
-    }
-    root
+    x
 }
 
-unsafe fn remove_min(mut root: &mut Node) -> (*mut Node, *mut Node) {
-    if root.left.is_null() {
-        return (null_mut(), root);
+unsafe fn fixup(mut x: &mut Node) -> &mut Node {
+    if color(x.right) == Red {
+        x = rotate_left(x);
     }
-    if color(root.left) == Black && color((*root.left).left) == Black {
-        root = move_red_left(root);
+    if color(x.left) == Red && color((*x.left).left) == Red {
+        x = rotate_right(x);
     }
-    let (handle, removed) = remove_min(&mut *root.left);
-    root.left = handle;
-    (fix_up(root), removed)
+    if color(x.left) == Red && color(x.right) == Red {
+        split_four_node(x);
+    }
+    x
 }
 
 unsafe fn rotate_right(x: &mut Node) -> &mut Node {
@@ -179,93 +177,81 @@ unsafe fn rotate_left(x: &mut Node) -> &mut Node {
     y
 }
 
-unsafe fn split_four_node(root: &mut Node) {
-    root.color = Red;
-    (*root.left).color = Black;
-    (*root.right).color = Black;
+unsafe fn split_four_node(x: &mut Node) {
+    x.color = Red;
+    (*x.left).color = Black;
+    (*x.right).color = Black;
 }
 
-unsafe fn join_two_nodes(root: &mut Node) {
-    root.color = Black;
-    (*root.left).color = Red;
-    (*root.right).color = Red;
+unsafe fn join_two_nodes(x: &mut Node) {
+    x.color = Black;
+    (*x.left).color = Red;
+    (*x.right).color = Red;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use rand::{rngs::StdRng, Rng, SeedableRng};
-    use std::collections::HashSet;
+    use std::collections::BTreeSet;
 
-    fn dump(root: *const Node) -> String {
-        unsafe fn dump_impl(root: *const Node, out: &mut String) {
-            if let Some(root) = root.as_ref() {
-                use std::fmt::Write as _;
-                out.push('(');
-                dump_impl(root.left, out);
-                write!(
-                    out,
-                    "\x1b[0;{}m{}\x1b[m",
-                    match root.color {
-                        Red => "31",
-                        Black => "30",
-                    },
-                    root.key,
-                )
-                .unwrap();
-                dump_impl(root.right, out);
-                out.push(')');
-            }
-        }
-        let mut out = String::new();
-        unsafe { dump_impl(root, &mut out) };
-        out
-    }
-
-    fn is_valid(root: *const Node) -> bool {
-        unsafe fn try_bh(root: *const Node) -> Option<usize> {
-            let Some(root) = root.as_ref() else {
+    fn is_valid(x: *const Node) -> bool {
+        unsafe fn is_valid_recurse(x: *const Node) -> Option<u8> {
+            let Some(root) = x.as_ref() else {
                 return Some(0);
             };
             if root.color == Red && color(root.right) == Red {
                 return None;
             }
-            let left = try_bh(root.left)?;
-            let right = try_bh(root.right)?;
-            if left != right {
+            let lbh = is_valid_recurse(root.left)?;
+            let rbh = is_valid_recurse(root.right)?;
+            if lbh != rbh {
                 return None;
             }
-            Some(left + usize::from(root.color == Black))
+            Some(lbh + u8::from(root.color == Black))
         }
-        unsafe { color(root) == Black && try_bh(root).is_some() }
+        unsafe { color(x) == Black && is_valid_recurse(x).is_some() }
+    }
+
+    fn collect(x: *const Node) -> Vec<i64> {
+        unsafe fn collect_recurse(x: *const Node, out: &mut Vec<i64>) {
+            if let Some(x) = x.as_ref() {
+                collect_recurse(x.left, out);
+                out.push(x.key);
+                collect_recurse(x.right, out);
+            }
+        }
+        let mut out = Vec::new();
+        unsafe { collect_recurse(x, &mut out) };
+        out
     }
 
     #[test]
     fn test_insert_remove() {
         let mut rng = StdRng::seed_from_u64(42);
-        for _ in 0..1000 {
+        for _ in 0..2000 {
             let q = 100;
             let insert_ratio = rng.random_range(0.0..=1.0);
             let key_lim = 30;
             let mut tree = Rbtree::default();
-            let mut hash_set = HashSet::new();
+            let mut btree_set = BTreeSet::new();
             for _ in 0..q {
                 let die = rng.random_range(0.0..=1.0);
                 let key = rng.random_range(0..key_lim);
                 if die <= insert_ratio {
                     eprintln!("Insert {key}");
-                    let result = tree.insert(key);
-                    let expected = hash_set.insert(key);
-                    assert_eq!(result, expected);
+                    tree.insert(key);
+                    btree_set.insert(key);
                 } else {
                     eprintln!("Remove {key}");
-                    let result = tree.remove(key);
-                    let expected = hash_set.remove(&key);
-                    assert_eq!(result, expected);
+                    tree.remove(key);
+                    btree_set.remove(&key);
                 }
-                eprintln!("tree = {}", dump(tree.root));
-                eprintln!();
                 assert!(is_valid(tree.root));
+                assert_eq!(
+                    collect(tree.root),
+                    btree_set.iter().copied().collect::<Vec<_>>().as_slice(),
+                );
             }
         }
     }
