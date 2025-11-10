0319a4ba396a6a755d15af23a3dc0d84108272fb
Nagata Kana <ngtkana@gmail.com>
2025-11-11 03:45:16 +0900

    SplayTreeDoubleRotationWithParent

--- bbst-comparison/benches/bbst_bench.rs
+++ bbst-comparison/benches/bbst_bench.rs
@@ -2,10 +2,12 @@ use bbst_comparison::avl_tree_by_box::AvlTreeByBox;
 use bbst_comparison::avl_tree_with_parent::AvlTreeWithParent;
 use bbst_comparison::bench_utils::{generate_initial_values, generate_queries, Query};
 use bbst_comparison::rb_tree_with_parent::RbTreeWithParent;
+use bbst_comparison::splay_tree_double_rotation_with_parent::SplayTreeDoubleRotationWithParent;
 use bbst_comparison::splay_tree_with_parent::SplayTreeWithParent;
 use bbst_comparison::unbalanced_tree_with_parent::UnbalancedTreeWithParent;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+#[allow(clippy::too_many_lines)]
 fn bench_avl_tree_operations(c: &mut Criterion) {
     let mut group = c.benchmark_group("bbst_comparison");
 
@@ -68,6 +70,24 @@ fn bench_avl_tree_operations(c: &mut Criterion) {
             let initial_values = generate_initial_values();
             let queries = generate_queries();
             let mut tree: SplayTreeWithParent = initial_values.into_iter().collect();
+            for &query in &queries {
+                match query {
+                    Query::Insert { index, value } => {
+                        tree.insert(black_box(index), black_box(value));
+                    }
+                    Query::Remove { index } => {
+                        tree.remove(black_box(index));
+                    }
+                }
+            }
+        });
+    });
+
+    group.bench_function("splay_double_rotation_with_parent", |b| {
+        b.iter(|| {
+            let initial_values = generate_initial_values();
+            let queries = generate_queries();
+            let mut tree: SplayTreeDoubleRotationWithParent = initial_values.into_iter().collect();
             for &query in &queries {
                 match query {
                     Query::Insert { index, value } => {
---
+++ bbst-comparison/src/splay_tree_double_rotation_with_parent.rs
@@ -0,0 +1,452 @@
+use std::{
+    cmp::Ordering,
+    ptr::{self, null_mut},
+};
+
+pub struct SplayTreeDoubleRotationWithParent {
+    root: *mut Node,
+}
+impl Default for SplayTreeDoubleRotationWithParent {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl SplayTreeDoubleRotationWithParent {
+    pub fn new() -> Self {
+        Self { root: null_mut() }
+    }
+    pub fn len(&self) -> usize {
+        unsafe { self.root.as_ref().map_or(0, |r| r.len) }
+    }
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+    pub fn insert(&mut self, index: usize, value: i32) {
+        unsafe {
+            let c = Box::leak(Box::new(Node {
+                parent: null_mut(),
+                left: null_mut(),
+                right: null_mut(),
+                value,
+                len: 1,
+                ht: 1,
+            }));
+            let (l, r) = split2(self.root, index);
+            self.root = merge3(l, c, r);
+        }
+    }
+    pub fn remove(&mut self, index: usize) -> i32 {
+        unsafe {
+            let (l, c, r) = split3(&mut *self.root, index);
+            let c = Box::from_raw(c);
+            self.root = merge2(l, r);
+            c.value
+        }
+    }
+}
+
+impl FromIterator<i32> for SplayTreeDoubleRotationWithParent {
+    fn from_iter<T: IntoIterator<Item = i32>>(iter: T) -> Self {
+        fn from_iter_recurse(values: &[i32]) -> *mut Node {
+            unsafe {
+                let n = values.len();
+                if n == 0 {
+                    return null_mut();
+                }
+                let l = from_iter_recurse(&values[..n / 2]);
+                let c = Box::leak(Box::new(Node {
+                    parent: null_mut(),
+                    left: null_mut(),
+                    right: null_mut(),
+                    value: values[n / 2],
+                    ht: 0,
+                    len: 0,
+                }));
+                let r = from_iter_recurse(&values[n / 2 + 1..]);
+                c.left = l;
+                c.right = r;
+                if let Some(l) = l.as_mut() {
+                    l.parent = c;
+                }
+                if let Some(r) = r.as_mut() {
+                    r.parent = c;
+                }
+                c.update();
+                c
+            }
+        }
+        let values: Vec<_> = iter.into_iter().collect();
+        let root = from_iter_recurse(&values);
+        Self { root }
+    }
+}
+
+pub struct Node {
+    parent: *mut Self,
+    left: *mut Self,
+    right: *mut Self,
+    value: i32,
+    len: usize,
+    ht: u8,
+}
+
+impl Node {
+    unsafe fn update(&mut self) {
+        self.len = 1;
+        self.ht = 1;
+        if let Some(l) = self.left.as_ref() {
+            self.len += l.len;
+            self.ht = self.ht.max(l.ht + 1);
+        }
+        if let Some(r) = self.right.as_ref() {
+            self.len += r.len;
+            self.ht = self.ht.max(r.ht + 1);
+        }
+    }
+}
+
+unsafe fn split2(x: *mut Node, index: usize) -> (*mut Node, *mut Node) {
+    if index == x.as_ref().map_or(0, |x| x.len) {
+        return (x, null_mut());
+    }
+    let (l, c, r) = split3(&mut *x, index);
+    (l, merge2(c, r))
+}
+
+unsafe fn split3(mut x: &mut Node, mut index: usize) -> (*mut Node, &mut Node, *mut Node) {
+    loop {
+        let llen = x.left.as_ref().map_or(0, |x| x.len);
+        match index.cmp(&llen) {
+            Ordering::Less => x = &mut *x.left,
+            Ordering::Equal => break,
+            Ordering::Greater => {
+                x = &mut *x.right;
+                index -= llen + 1;
+            }
+        }
+    }
+    splay(x);
+    let l = x.left;
+    let r = x.right;
+    x.left = null_mut();
+    x.right = null_mut();
+    if let Some(l) = l.as_mut() {
+        l.parent = null_mut();
+    }
+    if let Some(r) = r.as_mut() {
+        r.parent = null_mut();
+    }
+    x.update();
+    (l, x, r)
+}
+
+unsafe fn merge2(l: *mut Node, r: *mut Node) -> *mut Node {
+    let Some(mut r) = r.as_mut() else { return l };
+    while let Some(next) = r.left.as_mut() {
+        r = next;
+    }
+    splay(r);
+    r.left = l;
+    if let Some(l) = l.as_mut() {
+        l.parent = r;
+    }
+    r.update();
+    r
+}
+
+unsafe fn merge3(l: *mut Node, c: &mut Node, r: *mut Node) -> &mut Node {
+    &mut *merge2(l, merge2(c, r))
+}
+
+#[allow(clippy::too_many_lines)]
+unsafe fn splay(x: &mut Node) {
+    while let Some(p) = x.parent.as_mut() {
+        let Some(pp) = p.parent.as_mut() else {
+            if ptr::eq(p.left, x) {
+                x.parent = p.parent;
+                if let Some(p) = x.parent.as_mut() {
+                    if ptr::eq(p.right, p) {
+                        p.right = x;
+                    } else {
+                        p.left = x;
+                    }
+                }
+                p.left = x.right;
+                if let Some(c) = p.left.as_mut() {
+                    c.parent = p;
+                }
+                p.parent = x;
+                x.right = p;
+            } else if ptr::eq(p.right, x) {
+                x.parent = p.parent;
+                if let Some(p) = x.parent.as_mut() {
+                    if ptr::eq(p.right, p) {
+                        p.right = x;
+                    } else {
+                        p.left = x;
+                    }
+                }
+                p.right = x.left;
+                if let Some(c) = p.right.as_mut() {
+                    c.parent = p;
+                }
+                p.parent = x;
+                x.left = p;
+            } else {
+                unreachable!()
+            }
+            p.update();
+            x.update();
+            break;
+        };
+        x.parent = pp.parent;
+        if let Some(ppp) = pp.parent.as_mut() {
+            x.parent = ppp;
+            if ptr::eq(ppp.left, pp) {
+                ppp.left = x;
+            } else if ptr::eq(ppp.right, pp) {
+                ppp.right = x;
+            } else {
+                unreachable!();
+            }
+        }
+        match (ptr::eq(pp.left, p), ptr::eq(p.left, x)) {
+            (true, true) => {
+                pp.left = p.right;
+                p.left = x.right;
+                if let Some(a) = p.left.as_mut() {
+                    a.parent = p;
+                }
+                if let Some(a) = pp.left.as_mut() {
+                    a.parent = pp;
+                }
+                pp.parent = p;
+                p.right = pp;
+                x.right = p;
+            }
+            (true, false) => {
+                p.right = x.left;
+                pp.left = x.right;
+                if let Some(l) = x.left.as_mut() {
+                    l.parent = p;
+                }
+                if let Some(r) = x.right.as_mut() {
+                    r.parent = pp;
+                }
+                x.left = p;
+                x.right = pp;
+                pp.parent = x;
+            }
+            (false, true) => {
+                pp.right = x.left;
+                p.left = x.right;
+                if let Some(l) = x.left.as_mut() {
+                    l.parent = pp;
+                }
+                if let Some(r) = x.right.as_mut() {
+                    r.parent = p;
+                }
+                x.left = pp;
+                x.right = p;
+                pp.parent = x;
+            }
+            (false, false) => {
+                pp.right = p.left;
+                p.right = x.left;
+                if let Some(l) = p.left.as_mut() {
+                    l.parent = pp;
+                }
+                if let Some(r) = x.left.as_mut() {
+                    r.parent = p;
+                }
+                pp.parent = p;
+                p.left = pp;
+                x.left = p;
+            }
+        }
+        p.parent = x;
+        pp.update();
+        p.update();
+        x.update();
+    }
+}
+
+#[allow(dead_code)]
+fn pretty(x: *mut Node) -> String {
+    fn pretty_recurse(x: &Node, s: &mut String, overlines: &mut Vec<bool>) {
+        unsafe {
+            use std::fmt::Write;
+            let dir = match x.parent.as_ref() {
+                Some(p) if ptr::eq(p.left, x) => 1,
+                Some(p) if ptr::eq(p.right, x) => 2,
+                _ => 0,
+            };
+            if let Some(l) = x.left.as_ref() {
+                overlines.push(dir == 2);
+                pretty_recurse(l, s, overlines);
+                overlines.pop().unwrap();
+            }
+            writeln!(
+                s,
+                "{overlines}{corner}{branch} {padding} {value} {{ parent: {parent}, left: {left}, right: {right}, ht: {ht} }}",
+                overlines = overlines
+                    .iter()
+                    .map(|&b| if b { "│" } else { " " })
+                    .collect::<String>(),
+                corner = match dir {
+                    0 => "─",
+                    1 => "┌",
+                    2 => "└",
+                    _ => unreachable!(),
+                },
+                branch = match (x.left.as_ref(), x.right.as_ref()) {
+                    (None, None) => '╴',
+                    (Some(_), None) => '┘',
+                    (None, Some(_)) => '┐',
+                    (Some(_), Some(_)) => '┤',
+                },
+                padding = "┄".repeat(4_usize.saturating_sub(overlines.len())),
+                value = x.value,
+                parent = x.parent.as_ref().map_or_else(||"*".to_owned(), |x| x.value.to_string()),
+                left = x.left.as_ref().map_or_else(||"*".to_owned(), |x| x.value.to_string()),
+                right = x.right.as_ref().map_or_else(||"*".to_owned(), |x| x.value.to_string()),
+                ht = x.ht,
+            )
+            .unwrap();
+            if let Some(r) = x.right.as_ref() {
+                overlines.push(dir == 1);
+                pretty_recurse(r, s, overlines);
+                overlines.pop().unwrap();
+            }
+        }
+    }
+    unsafe {
+        if let Some(x) = x.as_ref() {
+            let mut s = String::new();
+            pretty_recurse(x, &mut s, &mut vec![]);
+            assert_eq!(s.pop().unwrap(), '\n');
+            s
+        } else {
+            "(empty)".to_owned()
+        }
+    }
+}
+
+impl crate::test_utils::TreeNode for Node {
+    fn left(&self) -> Option<&Self> {
+        unsafe { self.left.as_ref() }
+    }
+    fn right(&self) -> Option<&Self> {
+        unsafe { self.right.as_ref() }
+    }
+    fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+impl crate::test_utils::Validatable for Node {
+    const HAS_PARENT_POINTER: bool = true;
+
+    fn validate_balance(&self) -> bool {
+        true
+    }
+    fn parent(&self) -> Option<&Self> {
+        unsafe { self.parent.as_ref() }
+    }
+}
+
+impl crate::test_utils::Tree for SplayTreeDoubleRotationWithParent {
+    type Node = Node;
+
+    fn root(&self) -> Option<&Self::Node> {
+        unsafe { self.root.as_ref() }
+    }
+    fn len(&self) -> usize {
+        self.len()
+    }
+    fn insert(&mut self, index: usize, value: i32) {
+        self.insert(index, value);
+    }
+    fn remove(&mut self, index: usize) -> i32 {
+        self.remove(index)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test_utils::{self, Query};
+    use rand::{rngs::StdRng, Rng, SeedableRng};
+
+    #[test]
+    fn test_from_iter() {
+        let mut rng = StdRng::seed_from_u64(42);
+        for tid in 1..=200 {
+            eprintln!("==== Case #{tid}");
+            let n = rng.random_range(0..=20);
+            let value_lim = 20;
+            let vec = std::iter::repeat_with(|| rng.random_range(0..value_lim))
+                .take(n)
+                .collect::<Vec<_>>();
+            let tree: SplayTreeDoubleRotationWithParent = vec.iter().copied().collect();
+            eprintln!("vec = {vec:?}");
+            eprintln!("tree\n{}", pretty(tree.root));
+            test_utils::validate(&tree);
+            eprintln!("tree validated!");
+            assert_eq!(test_utils::collect(&tree), vec);
+        }
+    }
+
+    #[test]
+    fn test_random() {
+        let mut rng = StdRng::seed_from_u64(42);
+        for tid in 1..=200 {
+            let q = 200;
+            let len_max = rng.random_range(5..=50);
+            let value_lim = 20;
+            let mut n = 0_usize;
+            let queries: Vec<_> = std::iter::repeat_with(|| {
+                if rng.random_ratio(n as u32, len_max) {
+                    let index = rng.random_range(0..n);
+                    n -= 1;
+                    Query::Remove { index }
+                } else {
+                    let index = rng.random_range(0..=n);
+                    let value = rng.random_range(0..value_lim);
+                    n += 1;
+                    Query::Insert { index, value }
+                }
+            })
+            .take(q)
+            .collect();
+            let mut tree = SplayTreeDoubleRotationWithParent::new();
+            let mut vec = vec![];
+            for (qid, &query) in (1..).zip(&queries) {
+                eprintln!("==== Case #{tid}.{qid}: {query:?}");
+                match query {
+                    Query::Insert { index, value } => {
+                        tree.insert(index, value);
+                        vec.insert(index, value);
+                    }
+                    Query::Remove { index } => {
+                        let result = tree.remove(index);
+                        let expected = vec.remove(index);
+                        assert_eq!(result, expected);
+                    }
+                }
+                eprintln!("vec = {vec:?}");
+                eprintln!("tree\n{}", pretty(tree.root));
+                test_utils::validate(&tree);
+                eprintln!("tree validated!");
+                assert_eq!(test_utils::collect(&tree), vec);
+                eprintln!();
+            }
+        }
+    }
+
+    #[test]
+    fn analyze_tree_stats() {
+        test_utils::analyze_tree_stats::<SplayTreeDoubleRotationWithParent>();
+    }
+}
