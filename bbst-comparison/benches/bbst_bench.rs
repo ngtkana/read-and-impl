8a076bd36b94ac5c1de502ea3f532281a5cb6724
Nagata Kana <ngtkana@gmail.com>
2025-11-10 18:57:38 +0900

    rbtree bench

--- bbst-comparison/benches/bbst_bench.rs
+++ bbst-comparison/benches/bbst_bench.rs
@@ -1,12 +1,13 @@
 use bbst_comparison::avl_tree_by_box::AvlTreeByBox;
 use bbst_comparison::avl_tree_with_parent::AvlTreeWithParent;
 use bbst_comparison::bench_utils::{generate_initial_values, generate_queries, Query};
+use bbst_comparison::rb_tree_with_parent::RbTreeWithParent;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn bench_avl_tree_operations(c: &mut Criterion) {
-    let mut group = c.benchmark_group("avl_tree_comparison");
+    let mut group = c.benchmark_group("bbst_comparison");
 
-    group.bench_function("with_parent", |b| {
+    group.bench_function("avl_with_parent", |b| {
         b.iter(|| {
             let initial_values = generate_initial_values();
             let queries = generate_queries();
@@ -24,11 +25,29 @@ fn bench_avl_tree_operations(c: &mut Criterion) {
         });
     });
 
-    group.bench_function("by_box", |b| {
+    group.bench_function("avl_by_box", |b| {
         b.iter(|| {
             let initial_values = generate_initial_values();
             let queries = generate_queries();
             let mut tree: AvlTreeByBox = initial_values.into_iter().collect();
+            for &query in &queries {
+                match query {
+                    Query::Insert { index, value } => {
+                        tree.insert(black_box(index), black_box(value));
+                    }
+                    Query::Remove { index } => {
+                        tree.remove(black_box(index));
+                    }
+                }
+            }
+        });
+    });
+
+    group.bench_function("rb_with_parent", |b| {
+        b.iter(|| {
+            let initial_values = generate_initial_values();
+            let queries = generate_queries();
+            let mut tree: RbTreeWithParent = initial_values.into_iter().collect();
             for &query in &queries {
                 match query {
                     Query::Insert { index, value } => {
